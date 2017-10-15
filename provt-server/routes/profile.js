10e7c02b62f915a4180bed73330a94f5c48375c8
Luiz Aoqui <laoqui@ca.ibm.com>
2017-10-15 05:45:51 -0400

    remove profile schema

--- provt-server/routes/profile.js
+++ provt-server/routes/profile.js
@@ -3,16 +3,6 @@ const Profile = require('../models/profile');
 
 const router = express.Router();
 
-function profileFromParams(params) {
-  return {
-    _id: params.address,
-    name: params.name,
-    company: params.company,
-    webiste: params.webiste,
-    verified: params.verified,
-  };
-}
-
 router.get('/', (req, res) => {
   Profile.find(req.query)
     .then(profiles => res.send(profiles));
@@ -24,7 +14,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  const profile = new Profile(profileFromParams(req.body));
+  const profile = new Profile(req.body);
 
   profile.save()
     .then(result => res.send(result));
@@ -32,7 +22,7 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   Profile.findById(req.params.id)
-    .then(profile => profile.update(profileFromParams(req.body)))
+    .then(profile => profile.update(req.body))
     .then(result => res.send(result));
 });
 
